9cdb2bcb8531df3db2a85054cf4a56dc1989d7aa
koteswararao <koteswararao@KOTESWARARAO>
2024-04-17 13:59:22 -0400

    Add project files.

---
+++ CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,29 @@
+﻿using CatalogService.Database;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly DatabaseContext _db;
+
+        public CatalogController(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.Products.ToList();
+        }
+    }
+
+    
+}
+    
